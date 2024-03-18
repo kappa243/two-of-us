68c7289184f90e25e93b8b4dd294cd82e9438664
Szymon Idec <szymon.idec@protonmail.com>
2024-03-18 22:44:37 +0100

    converted game component as functional

--- app/src/app/game.tsx
+++ app/src/app/game.tsx
@@ -2,62 +2,52 @@
 
 import { GameBase } from "@/game/GameBase";
 import { Application, ApplicationOptions, Renderer } from "pixi.js";
-import { Component, ReactNode } from "react";
-
+import { useEffect, useRef } from "react";
 
 const options: Partial<ApplicationOptions> = {
   background: "#1099bb"
 };
 
-export default class Game extends Component {
+const Game = () => {
+  const canvasParentRef = useRef<HTMLDivElement>(null);
+  const app = useRef<Application<Renderer> | null>(null);
+  const game = useRef<GameBase | null>(null);
 
-  // React only - don't touch
-  _canvas_parent!: HTMLDivElement;
-  app!: Application<Renderer>;
-  game!: GameBase;
-
-  /** 
-   * Intializes renderer and attaches it's canvas to the DOM
-  */
-  async init() {
-    this._canvas_parent = document.getElementById("game") as HTMLDivElement;
-
-    await this.app.init({ resizeTo: this._canvas_parent, ...options });
-
-    if (this.app.renderer.canvas === undefined)
-      throw new Error("Canvas is undefined (renderer init probably failed)");
-
-    this._canvas_parent.appendChild(this.app.canvas);
-  }
-
-  componentDidMount() {
-    this.app = new Application();
-    this.game = new GameBase();
-
+  useEffect(() => {
+    const canvasParentRefCopy = canvasParentRef.current;
     (async () => {
-      await this.init();
-      await this.game.run(this.app);
+      app.current = new Application();
+      game.current = new GameBase();
+
+      if (!canvasParentRef.current) {
+        throw new Error("Canvas parent ref is null");
+      }
+  
+      await app.current?.init({ resizeTo: canvasParentRef.current, ...options });
+  
+      if (app.current?.renderer.canvas === undefined) {
+        throw new Error("Canvas is undefined (renderer init probably failed)");
+      }
+  
+      canvasParentRef.current.appendChild(app.current.canvas);
+      await game.current?.run(app.current);
+      
     })();
 
-  }
+    return () => {
+      if (canvasParentRefCopy) canvasParentRefCopy.textContent = "";
 
-  componentWillUnmount() {
-    this._canvas_parent.textContent = "";
+      if (game.current) game.current.stop();
+      if (app.current?.renderer) app.current.stop();
+    };
+  }, []);
 
-    if (this.game !== undefined)
-      this.game.stop();
+  return (
+    <div
+      ref={canvasParentRef}
+      className="h-dvh w-dvh bg-black"
+    />
+  );
+};
 
-    if (this.app.renderer !== undefined)
-      this.app.destroy();
-  }
-
-  render(): ReactNode {
-    return (
-      <div
-        className="h-dvh w-dvh bg-black"
-        id="game"
-      /> // this div.node cannot have any child nodes
-    );
-  }
-
-}
+export default Game;
